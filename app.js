a8544812616070875ee97857d1b8c259454cdbfe
aslamwebz <42026937+aslamwebz@users.noreply.github.com>
2018-12-24 00:15:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item',
+
+});
+
+// filter functions
+var filterFns = {
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
+
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
